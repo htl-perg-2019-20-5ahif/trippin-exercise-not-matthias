f6556edffde7b46523aeb3ebe51f3295f83f9f58
not-matthias <26800596+not-matthias@users.noreply.github.com>
2019-10-10 16:55:39 +0200

    Added ultra small version

--- tripping/console/Program.cs
+++ tripping/console/Program.cs
@@ -1,59 +1,9 @@
-﻿using System;
-using System.IO;
-using System.Net.Http;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
-
-namespace console
-{
-    class Program
-    {
-        private static readonly HttpClient client = new HttpClient() { BaseAddress = new Uri("https://services.odata.org/TripPinRESTierService/(S(f3u1x5y4m11keyabeqbctnqd))/") };
-
-        static async Task Main(string[] args)
-        {
-            var users = JsonSerializer.Deserialize<User[]>(await File.ReadAllTextAsync("users.json"));
-
-            foreach (var user in users)
-            {
-                if (!await DoesUserExist(user))
-                {
-                    Console.WriteLine($"Adding user {user.UserName}.");
-                    await AddUser(user);
-                }
-            }
-        }
-
-        public static async Task<bool> DoesUserExist(User user)
-        {
-            return (await client.GetAsync($"People('{user.UserName}')")).IsSuccessStatusCode;
-        }
-
-        public static async Task<bool> AddUser(User user)
-        {
-            var newUser = new
-            {
-                UserName = user.UserName,
-                FirstName = user.FirstName,
-                LastName = user.LastName,
-                Emails = new[] {
-                    user.Email
-                },
-                AddressInfo = new[] {
-                    new {
-                        Address = user.Address,
-                        City = new  {
-                            Name = user.CityName,
-                            CountryRegion = user.Country,
-                            Region = "unknown"
-                        }
-                    }
-                }
-            };
-
-            var content = new StringContent(JsonSerializer.Serialize(newUser), Encoding.UTF8, "application/json");
-            return (await client.PostAsync("People", content)).IsSuccessStatusCode;
-        }
+﻿class User { public string UserName, FirstName, LastName, Email, Address, CityName, Country; }
+class Program {
+    private static readonly System.Net.Http.HttpClient client = new System.Net.Http.HttpClient() { BaseAddress = new System.Uri("https://services.odata.org/TripPinRESTierService/(S(f3u1x5y4m11keyabeqbctnqd))/") };
+    static async System.Threading.Tasks.Task Main(string[] args) {
+        foreach (var user in System.Text.Json.JsonSerializer.Deserialize<User[]>(await System.IO.File.ReadAllTextAsync("users.json"))) // Loop through every user read from the file
+            if (!(await client.GetAsync($"People('{user.UserName}')")).IsSuccessStatusCode) // Check if user exists
+                await client.PostAsync("People", new System.Net.Http.StringContent(System.Text.Json.JsonSerializer.Serialize(new { user.UserName, user.FirstName, user.LastName, Emails = new[] { user.Email }, AddressInfo = new[] { new { user.Address, City = new { Name = user.CityName, CountryRegion = user.Country, Region = "unknown" } } } }), System.Text.Encoding.UTF8, "application/json"));
     }
 }
